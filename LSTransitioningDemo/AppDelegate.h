17f5908998b8d834c77c08e423dc6ec070821b44
gankai <gk@gankaideimac.local>
2018-11-15 17:43:43 +0800

    Initial Commit

---
+++ LSTransitioningDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSTransitioningDemo
+//
+//  Created by gankai on 2018/11/15.
+//  Copyright © 2018年 laoSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
